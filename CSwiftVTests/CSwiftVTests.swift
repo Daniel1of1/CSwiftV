636e82e6da78d1d3400bbe29363e3d06985a25f5
Daniel Haight <confidence.designed@gmail.com>
2014-09-06 13:42:26 +0100

    comment on test cases wrt rfc4180

--- CSwiftVTests/CSwiftVTests.swift
+++ CSwiftVTests/CSwiftVTests.swift
@@ -26,10 +26,16 @@ public let withQuotesInQuotes = "Year,Make,Model,Description,Price\r\n1997,Ford,
 public let withNewLinesInQuotes = "Year,Make,Model,Description,Price\r\n1997,Ford,\"E350\",descrition,3000.00\r\n1999,Chevy,Venture,\"another, \"\"amazing\"\",\r\ndescription\r\n\",4900.00\r\n"
 
 class CSwiftVTests: XCTestCase {
-   // modelling from http://tools.ietf.org/html/rfc4180#section-2
     
     var testString: String!
+
+   // modelling from http://tools.ietf.org/html/rfc4180#section-2
     
+    //1.  Each record is located on a separate line, delimited by a line
+    //break (CRLF).  For example:
+    
+    //aaa,bbb,ccc CRLF
+    //zzz,yyy,xxx CRLF
     func testThatItParsesLinesSeperatedByNewLines() {
         testString = newLineSeparation
 
@@ -43,6 +49,12 @@ class CSwiftVTests: XCTestCase {
         XCTAssertEqual(arrayUnderTest, expectedArray)
 
     }
+    
+    //2.  The last record in the file may or may not have an ending line
+    //break.  For example:
+    
+    //aaa,bbb,ccc CRLF
+    //zzz,yyy,xxx
     
     func testThatItParsesLinesSeperatedByNewLinesWithoutNewLineAtEnd() {
 
@@ -59,6 +71,17 @@ class CSwiftVTests: XCTestCase {
         
     }
     
+    //3.  There maybe an optional header line appearing as the first line
+    //of the file with the same format as normal record lines.  This
+    //header will contain names corresponding to the fields in the file
+    //and should contain the same number of fields as the records in
+    //the rest of the file (the presence or absence of the header line
+    //should be indicated via the optional "header" parameter of this
+    //MIME type).  For example:
+    
+    //field_name,field_name,field_name CRLF
+    //aaa,bbb,ccc CRLF
+    //zzz,yyy,xxx CRLF
     func testThatItParsesHeadersCorrectly() {
         
         testString = newLineSeparationNoEnd
@@ -71,6 +94,13 @@ class CSwiftVTests: XCTestCase {
         
     }
     
+    // still 3. in RFC. This is the first decision we make in
+    // api design with regards to headers. Currently if nothing
+    // is passed in to the `headers` parameter (as is the case)
+    // with the convenience initialiser. We assume that the csv
+    // contains headers. If the headers are passed in, then we
+    // assume that the csv file does not contain them and expect
+    // it to be parsed accordingly.
     func testThatItParsesRowsWithoutHeaders() {
         
         testString = withoutHeader
@@ -88,6 +118,25 @@ class CSwiftVTests: XCTestCase {
         
     }
     
+    
+//    4.  Within the header and each record, there may be one or more
+//    fields, separated by commas.  Each line should contain the same
+//    number of fields throughout the file.  Spaces are considered part
+//    of a field and should not be ignored.  The last field in the
+//    record must not be followed by a comma.  For example:
+//    
+//    aaa,bbb,ccc
+//    
+//    This is covered by previous test cases since there are spaces in 
+//    fields and no commas at the end of the lines
+//    
+//    5.  Each field may or may not be enclosed in double quotes (however
+//    some programs, such as Microsoft Excel, do not use double quotes
+//    at all).  If fields are not enclosed with double quotes, then
+//    double quotes may not appear inside the fields.  For example:
+//    
+//    "aaa","bbb","ccc" CRLF
+//    zzz,yyy,xxx
     func testThatItParsesFieldswithQuotes() {
         
         testString = withRandomQuotes
@@ -103,6 +152,13 @@ class CSwiftVTests: XCTestCase {
         
     }
     
+//    6.  Fields containing line breaks (CRLF), double quotes, and commas
+//    should be enclosed in double-quotes.  For example:
+//    
+//    "aaa","b CRLF
+//    bb","ccc" CRLF
+//    zzz,yyy,xxx
+
     func testThatItParsesFieldswithCommasInQuotes() {
         
         testString = withCommasInQuotes
@@ -112,21 +168,6 @@ class CSwiftVTests: XCTestCase {
         let expectedArray = [
             ["1997","Ford","E350","descrition","3000.00"],
             ["1999","Chevy","Venture","another, amazing, description","4900.00"]
-        ]
-        
-        XCTAssertEqual(arrayUnderTest, expectedArray)
-        
-    }
-
-    func testThatItParsesFieldswithQuotesInQuotes() {
-        
-        testString = withQuotesInQuotes
-        
-        let arrayUnderTest =  CSwiftV(String: testString).rows as NSArray
-        
-        let expectedArray = [
-            ["1997","Ford","E350","descrition","3000.00"],
-            ["1999","Chevy","Venture","another, \"amazing\", description","4900.00"]
         ]
         
         XCTAssertEqual(arrayUnderTest, expectedArray)
@@ -147,5 +188,26 @@ class CSwiftVTests: XCTestCase {
         XCTAssertEqual(arrayUnderTest, expectedArray)
         
     }
+    
+//    7.  If double-quotes are used to enclose fields, then a double-quote
+//    appearing inside a field must be escaped by preceding it with
+//    another double quote.  For example:
+//    
+//    "aaa","b""bb","ccc"
 
+    func testThatItParsesFieldswithQuotesInQuotes() {
+        
+        testString = withQuotesInQuotes
+        
+        let arrayUnderTest =  CSwiftV(String: testString).rows as NSArray
+        
+        let expectedArray = [
+            ["1997","Ford","E350","descrition","3000.00"],
+            ["1999","Chevy","Venture","another, \"amazing\", description","4900.00"]
+        ]
+        
+        XCTAssertEqual(arrayUnderTest, expectedArray)
+        
+    }
+    
 }
