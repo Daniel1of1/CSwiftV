d4afdc7672a3df8c53cf4de24d0608322191653d
Daniel Haight <confidence.designed@gmail.com>
2015-05-04 15:41:01 +0100

    add iOS and OS X framework targets

---
+++ CSwiftVIOS/CSwiftVIOS.h
@@ -0,0 +1,19 @@
+//
+//  CSwiftVIOS.h
+//  CSwiftVIOS
+//
+//  Created by Daniel Haight on 04/05/2015.
+//  Copyright (c) 2015 ManyThings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSwiftVIOS.
+FOUNDATION_EXPORT double CSwiftVIOSVersionNumber;
+
+//! Project version string for CSwiftVIOS.
+FOUNDATION_EXPORT const unsigned char CSwiftVIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSwiftVIOS/PublicHeader.h>
+
+
