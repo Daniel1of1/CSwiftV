f8e6795ee16117fb06c24a42e388d721e88b72ee
Pierpaolo Frasa <pfrasa@gmail.com>
2019-04-25 19:34:24 +0200

    add products array to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "CSwiftV",
+    products: [
+      .library(name: "CSwiftV", targets: ["CSwiftV"])
+    ],
     targets: [
       .target(
           name: "CSwiftV",
