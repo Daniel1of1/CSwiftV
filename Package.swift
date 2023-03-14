5c82b6afcc410f57d2f7c03dba9e6e40bebd5856
Will Jessop <wjessop@gmail.com>
2023-03-13 23:40:10 -0400

    updating package.swift for Xcode 14

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CSwiftV",
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CSwiftV",
-            targets: ["CSwiftV"]),
+            targets: ["CSwiftV"])
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "CSwiftV",
@@ -15,5 +19,6 @@ let package = Package(
         .testTarget(
             name: "CSwiftVTests",
             dependencies: ["CSwiftV"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
